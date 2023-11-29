bf108de48f6e28ea51b615fe642b39afb1d77689
shashank8904 <152384914+shashank8904@users.noreply.github.com>
2023-11-29 15:09:52 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import redirect,render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+def home(request):
+    return render(request, "authentication/index.html") 
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST.get('fname','')
+        lname = request.POST.get('lname','')
+        email = request.POST.get('email','')
+        pass1 = request.POST.get('pass1','')
+        pass2 = request.POST.get('pass2','')
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request,"Your Account has been Successfully Created.")
+
+        return redirect('signup')
+
+
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+
+    if request.method =="POST":
+        username = request.POST['username']
+        pass1 = request.POST.get('pass1','')
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authenticate/index.html",{'fname': fname})
+
+        else:
+            message.error(request,"Bad Credentials!")
+            return redirect('home')
+    
+
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    message.success(request, "Logged Out Successfully")
+    return redirect('home')
+
